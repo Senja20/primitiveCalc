882d5e23d150ee59742ce3b058482647f562ee34
Yauhen Yavorski <yauheny@uia.no>
2022-06-12 13:54:26 +0200

    feat: assembly code added

---
+++ build/debug.d.ts
@@ -0,0 +1,34 @@
+/**
+ * assembly/index/divide
+ * @param a `i32`
+ * @param b `i32`
+ * @returns `i32`
+ */
+export declare function divide(a: number, b: number): number;
+/**
+ * assembly/index/multiply
+ * @param a `i32`
+ * @param b `i32`
+ * @returns `i32`
+ */
+export declare function multiply(a: number, b: number): number;
+/**
+ * assembly/index/subtract
+ * @param a `i32`
+ * @param b `i32`
+ * @returns `i32`
+ */
+export declare function subtract(a: number, b: number): number;
+/**
+ * assembly/index/add
+ * @param a `i32`
+ * @param b `i32`
+ * @returns `i32`
+ */
+export declare function add(a: number, b: number): number;
+/**
+ * assembly/index/fizzbuzz
+ * @param n `i32`
+ * @returns `~lib/string/String | null`
+ */
+export declare function fizzbuzz(n: number): string | null;
